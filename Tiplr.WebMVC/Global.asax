05e752511266e6e3e421d999edaad31243c0f5e0
John Peters <petersjohn0109@gmail.com>
2021-10-22 20:15:42 -0400

    Add project files.

---
+++ Tiplr.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tiplr.WebMVC.MvcApplication" Language="C#" %>
---
+++ Tiplr.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tiplr.WebMVC.Startup))]
+namespace Tiplr.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
